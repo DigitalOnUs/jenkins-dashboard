49c12b7621026b4dfc71efdcf13954fb5ce23396
Ricardo Lopez Hernandez <rlopez@e-healthtracker.com>
2018-01-30 18:02:11 -0600

    - Start bootstrap app

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: 'dashboard', title: 'Create Connection',  icon: 'dashboard', class: '' },
+    { path: 'upgrade', title: 'Images',  icon:'person', class: '' },
+    { path: 'table-list', title: 'Jenkins',  icon:'content_paste', class: '' },
+    { path: 'typography', title: 'Pipeline Generator',  icon:'library_books', class: '' }
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
