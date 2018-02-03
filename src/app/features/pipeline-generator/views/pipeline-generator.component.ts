bde2d96fb911b252c98f5043dab5b7adbda8b833
Ricardo Lopez Hernandez <rlopez@e-healthtracker.com>
2018-02-02 22:46:32 -0600

    - Minor features

--- src/app/features/pipeline-generator/views/pipeline-generator.component.ts
+++ src/app/features/pipeline-generator/views/pipeline-generator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { toast } from "angular2-materialize";
 
 declare const $: any;
 
@@ -10,18 +11,59 @@ declare const $: any;
 export class PipelineGeneratorComponent implements OnInit {
   form: FormGroup;
   optionsPipeline: any[];
-
+  isAddingOption: boolean;
+  dataToSentBackend: any[];
   constructor(fb: FormBuilder) {
+    $(document).ready(function() {
+      $(".collapsible").collapsible();
+    });
     this.form = fb.group({
       options: ["", Validators.required]
     });
   }
 
   ngOnInit(): void {
+    //TODO: ask for this structure to the backend
     this.optionsPipeline = [
-      { name: "one" },
-      { name: "two" },
-      { name: "three" }
+      {
+        name: "I.T",
+        description: "Lorem ipsum dolor sit amet."
+      },
+      {
+        name: "Compile",
+        description: "Lorem ipsum dolor sit amet"
+      },
+      {
+        name: "Deploy",
+        description: "Lorem ipsum dolor sit amet."
+      },{
+        name: "Build",
+        description: "Lorem ipsum dolor sit amet."
+      },
+      {
+        name: "Test",
+        description: "Lorem ipsum dolor sit amet."
+      }
     ];
+    this.dataToSentBackend = [];
+  }
+
+  addAction() {
+    if (!this.isAlreadyAdded(this.form.value)) {
+      this.dataToSentBackend.push(Object.assign({}, this.form.value));
+    }
+    this.form.reset();
+  }
+
+  isAlreadyAdded(data: any): boolean {
+    let found = false;
+    for (let i = 0; i < this.dataToSentBackend.length; i++) {
+      if (this.dataToSentBackend[i].options === data.options) {
+        found = true;
+        toast("Action already added", 3000);
+        break;
+      }
+    }
+    return found;
   }
 }
