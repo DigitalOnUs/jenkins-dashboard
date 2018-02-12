c10d5836eba2c19d9001f951ae9fb16137a5e641
Ricardo Lopez Hernandez <rlopez@e-healthtracker.com>
2018-02-12 15:41:56 -0600

    - Add create project view

--- src/app/views/projects/projects-view.component.ts
+++ src/app/views/projects/projects-view.component.ts
@@ -1,46 +1,64 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 declare const $: any;
-import swal from "sweetalert";
+import swal from 'sweetalert';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-projects-view",
-  templateUrl: "./projects-view.component.html"
+  selector: 'app-projects-view',
+  templateUrl: './projects-view.component.html'
 })
 export class ProjectsViewComponent implements OnInit {
   regionToNewProjectForm: any[];
   projects: any[];
+  form: FormGroup;
+  //TODO: this id would get it from backed
+  id: number ;
 
   ngOnInit(): void {
+    this.id = 2;
+    //TODO: Catalog from backend
     this.regionToNewProjectForm = [
-      { name: "USA" },
-      { name: "Mexico" },
-      { name: "India" }
+      { name: 'USA' },
+      { name: 'Mexico' },
+      { name: 'India' }
     ];
-
+    //TODO: Get request from backend
     this.projects = [
       {
         id: 1,
-        name: "Project 1",
+        name: 'Project 1',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium culpa doloremque eligendi et hic id, obcaecati quam sit? Accusamus consectetur cum eaque eos excepturi explicabo nesciunt pariatur quasi quisquam sint?'
       },
       {
         id: 2,
-        name: "Project 2",
+        name: 'Project 2',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium culpa doloremque eligendi et hic id, obcaecati quam sit? Accusamus consectetur cum eaque eos excepturi explicabo nesciunt pariatur quasi quisquam sint?'
       }
     ];
   }
 
-  constructor() {
+  constructor(fb: FormBuilder) {
     $(document).ready(function() {
       // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-      $(".modal").modal();
+      $('.modal').modal();
+    });
+    this.form = fb.group({
+      name: ['', Validators.required],
+      region: ['', Validators.required]
     });
   }
 
-  addNewProject(){
-    console.log("adding new project")
+  addNewProject() {
+    ++ this.id;
+    const object = {
+      id: this.id,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium culpa doloremque eligendi et hic id, obcaecati quam sit? Accusamus consectetur cum eaque eos excepturi explicabo nesciunt pariatur quasi quisquam sint?'
+    };
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium culpa doloremque eligendi et hic id, obcaecati quam sit? Accusamus consectetur cum eaque eos excepturi explicabo nesciunt pariatur quasi quisquam sint?';
+    this.projects.push(Object.assign({}, this.form.value, object));
+    this.form.reset();
   }
 }
--- src/app/views/projects/projects-view.module.ts
+++ src/app/views/projects/projects-view.module.ts
@@ -1,12 +1,13 @@
-import { ProjectsViewComponent } from './projects-view.component';
+import { ProjectsViewComponent } from "./projects-view.component";
 import { NgModule } from "@angular/core";
-import {RouterModule} from "@angular/router";
-import {MaterializeModule} from "angular2-materialize";
-import {CommonModule} from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MaterializeModule } from "angular2-materialize";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-    imports: [RouterModule,MaterializeModule, CommonModule],
-    declarations:[ProjectsViewComponent],
-    exports: [ProjectsViewComponent]
+  imports: [RouterModule, MaterializeModule, CommonModule, ReactiveFormsModule],
+  declarations: [ProjectsViewComponent],
+  exports: [ProjectsViewComponent]
 })
 export class ProjectsViewModule {}
