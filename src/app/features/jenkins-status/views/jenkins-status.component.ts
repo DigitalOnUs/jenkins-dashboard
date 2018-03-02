86a0e63255eb187b8faeb75395f0fb9600d1e71d
Richard Hern <rolzing.richardo@gmail.com>
2018-03-02 12:01:17 -0600

    - Add http call on interval for jenkins-status  * Add conditions for show only the current jenkin status * Add jenkins-status service

--- src/app/features/jenkins-status/views/jenkins-status.component.ts
+++ src/app/features/jenkins-status/views/jenkins-status.component.ts
@@ -1,12 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { JenkinStatusService } from './../../../services/jenkin-status.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-jenkins-status',
     templateUrl: 'jenkins-status.component.html'
 })
 
-export class JenkinsStatusComponent implements OnInit {
-    constructor() { }
+export class JenkinsStatusComponent implements OnInit, OnDestroy {
+    isGettingStatus: boolean;
+    status: any;
+    message: any;
+    interval: any;
+    constructor(private jenkinStatusService: JenkinStatusService) {
+        this.isGettingStatus = true;
+     }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        /**
+         * Make an Http every 
+         */
+        this.interval = setInterval(() => {
+            this.getStatus();
+        }, 1000);
+    }
+
+    ngOnDestroy() {
+        /**
+         * When this view is destroyed we clear every http call
+         */
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+       }
+
+    getStatus(){
+        this.isGettingStatus = true;
+        this.jenkinStatusService.getJenkinStatus().subscribe((response) => {
+            this.status = response.status;
+            this.message = response.message;
+            this.isGettingStatus = false;
+            console.log(this.status);
+        }, (err) => {
+            this.isGettingStatus = false;
+            console.error(err);
+        })
+    }
 }
--- src/app/features/jenkins-status/views/jenkins-status.module.ts
+++ src/app/features/jenkins-status/views/jenkins-status.module.ts
@@ -1,11 +1,13 @@
+import { JenkinStatusService } from './../../../services/jenkin-status.service';
 import { NgModule } from '@angular/core';
 
 import { JenkinsStatusComponent } from './jenkins-status.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [],
+    imports: [CommonModule],
     exports: [JenkinsStatusComponent],
     declarations: [JenkinsStatusComponent],
-    providers: [],
+    providers: [JenkinStatusService],
 })
 export class JenkinsStatusModule { }
