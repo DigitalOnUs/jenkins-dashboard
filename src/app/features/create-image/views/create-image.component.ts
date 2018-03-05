9804b5d6ef74d8ba25bdee7f2cfa896bf3b2e49f
Richard Hern <rolzing.richardo@gmail.com>
2018-03-05 11:49:56 -0600

    - Add switches and button submit

--- src/app/features/create-image/views/create-image.component.ts
+++ src/app/features/create-image/views/create-image.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { toast } from "angular2-materialize";
+declare const $: any;
 @Component({
     selector: 'app-create-image',
     templateUrl: './create-image.component.html'
 })
 
 export class CreateImageComponent implements OnInit {
-    constructor() { }
+    form: FormGroup;
 
-    ngOnInit() { }
+    constructor(private fb: FormBuilder) { 
+        this.form = fb.group({
+            jenkins: ['', Validators.required],
+            azure: ['', Validators.required],
+            slave: ['', Validators.required],
+            garrit: ['', Validators.required],
+            nexus: ['', Validators.required]
+        });
+    }
+
+    ngOnInit() {
+        $(document).ready(function(){
+            $('ul.tabs').tabs();
+          });
+                
+    }
+
+    sendToBack(){
+        toast('Success', 3000, 'rounded')
+    }
 }
--- src/app/features/create-image/views/create-image.module.ts
+++ src/app/features/create-image/views/create-image.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CreateImageComponent } from './create-image.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterializeModule } from 'angular2-materialize';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [],
+    imports: [ReactiveFormsModule, CommonModule, MaterializeModule, FormsModule],
     exports: [CreateImageComponent],
     declarations: [CreateImageComponent]
 })
