398c0f9567eea63f02aa05838647c5d1bf06b95b
Richard Hern <rolzing.richardo@gmail.com>
2018-03-07 20:15:45 -0600

    - Add create connection template.

--- src/app/features/connections/views/new-connection/new-connection-view.component.ts
+++ src/app/features/connections/views/new-connection/new-connection-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { toast } from "angular2-materialize";
 
 
 @Component({
@@ -6,8 +8,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-connection-view.component.html'
 })
 export class NewConnectionViewComponent implements OnInit {
+  form: FormGroup;
+  providers: any[];
 
-  constructor() { }
-  ngOnInit() {}
+  constructor(fb: FormBuilder) {
+      this.form = fb.group({
+          provider: ['', Validators.required],
+          accessKey: ['', Validators.required],
+          secret: ['', Validators.required]
+      });
+
+  }
+  ngOnInit() {
+      this.providers = [{
+          'id': 1,
+          'name': 'Azure'
+      }, {
+          'id': 2,
+          'name': 'Amazon Web Services'
+      }, {
+          'id': 3,
+          'name': 'Nexus'
+      }];
+  }
+
+  save(){
+      toast('Save', 3000, 'rounded')
+  }
 
 }
--- src/app/features/connections/views/new-connection/new-connection-view.module.ts
+++ src/app/features/connections/views/new-connection/new-connection-view.module.ts
@@ -1,7 +1,11 @@
 import { NewConnectionViewComponent } from './new-connection-view.component';
 import { NgModule } from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {MaterializeModule} from "angular2-materialize";
 
 @NgModule({
+    imports: [ReactiveFormsModule, CommonModule, MaterializeModule, FormsModule],
     declarations:[NewConnectionViewComponent],
     exports: [NewConnectionViewComponent]
 })
