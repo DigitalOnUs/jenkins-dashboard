ff98f4f9137c7f00807e59714474396965676cde
Richard Hern <rolzing.richardo@gmail.com>
2018-03-07 19:31:37 -0600

    - Add projectId to component.

--- src/app/features/create-image/views/create-image.component.ts
+++ src/app/features/create-image/views/create-image.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { toast } from 'angular2-materialize';
+import {ActivatedRoute} from '@angular/router';
 declare const $: any;
+
 @Component({
-    selector: 'app-create-image',
-    templateUrl: './create-image.component.html'
+  selector: 'app-create-image',
+  templateUrl: './create-image.component.html'
 })
-
 export class CreateImageComponent implements OnInit {
-    form: FormGroup;
+  form: FormGroup;
+  projectId: number;
 
-    constructor(private fb: FormBuilder) {
-        this.form = fb.group({
-            jenkins: [false, Validators.required],
-            azure: [false, Validators.required],
-            slave: [false, Validators.required],
-            garrit: [false, Validators.required],
-            nexus: [false, Validators.required]
-        });
-    }
+  constructor(private fb: FormBuilder,
+              private router: ActivatedRoute) {
+    this.form = fb.group({
+      jenkins: [false, Validators.required],
+      azure: [false, Validators.required],
+      slave: [false, Validators.required],
+      garrit: [false, Validators.required],
+      nexus: [false, Validators.required]
+    });
+  }
 
-    ngOnInit() {
-        $(document).ready(function(){
-            $('ul.tabs').tabs();
-          });
-    }
+  ngOnInit() {
+      this.router.parent.parent.params.subscribe((params) => this.projectId = params.id);
+    $(document).ready(function() {
+      $('ul.tabs').tabs();
+    });
+  }
 
-    sendToBack(){
-        toast('Success', 3000, 'rounded')
-    }
+  sendToBack() {
+    toast('Success', 3000, 'rounded');
+  }
 }
