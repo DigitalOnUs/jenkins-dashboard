9fdeac90276ec950c8064026db682264a8eb0f88
Richard Hern <rolzing.richardo@gmail.com>
2018-03-08 19:16:33 -0600

    - Add image service for http calls. 	* Add save image configuration.

--- src/app/features/create-image/views/create-image.component.ts
+++ src/app/features/create-image/views/create-image.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { toast } from 'angular2-materialize';
 import {ActivatedRoute} from '@angular/router';
+import {ImageService} from "../../../services/image.service";
 declare const $: any;
 
 @Component({
@@ -13,13 +14,14 @@ export class CreateImageComponent implements OnInit {
   projectId: number;
 
   constructor(private fb: FormBuilder,
-              private router: ActivatedRoute) {
+              private router: ActivatedRoute,
+              private imageService: ImageService) {
     this.form = fb.group({
-      jenkins: [false, Validators.required],
-      azure: [false, Validators.required],
-      slave: [false, Validators.required],
-      garrit: [false, Validators.required],
-      nexus: [false, Validators.required]
+      jenkins: ['', Validators.required],
+      azure: ['', Validators.required],
+      slave: ['', Validators.required],
+      gerrit: ['', Validators.required],
+      nexus: ['', Validators.required]
     });
   }
 
@@ -28,9 +30,76 @@ export class CreateImageComponent implements OnInit {
     $(document).ready(function() {
       $('ul.tabs').tabs();
     });
+    this.imageService.getListAllImages(this.projectId).subscribe((response) => console.log(response));
   }
 
-  sendToBack() {
-    toast('Success', 3000, 'rounded');
+  sendToBack(name: any) {
+    switch (name) {
+        case 'jenkins': {
+          const obj = {
+              'name': name,
+              'eneabled': this.form.controls.jenkins.value
+          };
+          this.imageService.postNewImage(obj, this.projectId).subscribe((response) => {
+              toast('Success', 3000, 'rounded');
+          }
+              , (err) => {
+              this.form.controls.jenkins.setValue(false);
+              });
+        }
+        break;
+        case 'azure': {
+            const obj = {
+                'name': name,
+                'eneabled': this.form.controls.azure.value
+            };
+            this.imageService.postNewImage(obj, this.projectId).subscribe((response) => {
+                toast('Success', 3000, 'rounded');
+            } , (err) => {
+                toast('Failed', 3000, 'rounded');
+                this.form.controls.azure.setValue(false);
+            });
+        }
+        break;
+        case 'slave': {
+            const obj = {
+                'name': name,
+                'eneabled': this.form.controls.slave.value
+            };
+            this.imageService.postNewImage(obj, this.projectId).subscribe((response) => {
+                toast('Success', 3000, 'rounded');
+            } , (err) => {
+                toast('Failed', 3000, 'rounded');
+                this.form.controls.slave.setValue(false);
+            });
+        }
+        break;
+        case 'gerrit': {
+            const obj = {
+                'name': name,
+                'eneabled': this.form.controls.gerrit.value
+            };
+            this.imageService.postNewImage(obj, this.projectId).subscribe((response) => {
+                toast('Success', 3000, 'rounded');
+            } , (err) => {
+                toast('Failed', 3000, 'rounded');
+                this.form.controls.gerrit.setValue(false);
+            });
+        }
+        break;
+        case 'nexus': {
+            const obj = {
+                'name': name,
+                'eneabled': this.form.controls.nexus.value
+            };
+            this.imageService.postNewImage(obj, this.projectId).subscribe((response) => {
+                toast('Success', 3000, 'rounded');
+            } , (err) => {
+                toast('Failed', 3000, 'rounded');
+                this.form.controls.nexus.setValue(false);
+            });
+        }
+        break;
+    }
   }
 }
